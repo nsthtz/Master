eaee2eb6c8a697202fcab4c63f0915a8f4260c70
nsthtz <rjinswand@gmail.com>
2019-07-02 21:30:23 +0200

    Results generated

--- IP/JuMP-dev-2019-tutorial-master/passport-index-dataset/processing/to_tidy.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-input = 'passport-index-iso3-matrix.csv'
-output = 'passport-index-iso3-tidy.csv'
-
-df = pd.read_csv(input, index_col=0)
-df = df.stack()
-
-df.to_csv(output, header=True)
